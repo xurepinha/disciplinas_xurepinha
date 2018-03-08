04dfa2dc36396b0345f8a953327bd947b1f6427a
Xurepinha <tgv_rock@yahoo.com.br>
2018-03-08 16:31:44 -0300

    Página show para disciplina/id criada

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -6,7 +6,7 @@
 
 <ul>
 @foreach ($disciplinas as $disciplina)
-    <li>{{ $disciplina->titulo }} </li>
+    <li><a href="/disciplinas/{{ $disciplina->id}}"> {{ $disciplina->titulo }}</a> </li>
 @endforeach
 </ul>
 
